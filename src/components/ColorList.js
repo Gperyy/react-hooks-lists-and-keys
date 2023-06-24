808fd324301f1f364aae3ca33555e688faa64d21
Gulperi Ozbulut <gulperiozblt@gmail.com>
2023-06-24 14:42:26 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,17 +10,22 @@ function ColorList() {
     "hotpink",
   ];
 
-  return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
-    </div>
+
+  const users = [
+    { id: 1, firstName: "Duane", lastName: "Watson" },
+    { id: 2, firstName: "Allan", lastName: "Johnson" },
+  ];
+
+  const userHeadings = users.map((user) => {
+    return <h1 key = { user.id } > { user.firstName } < /h1>;
+  });
+  return ( <
+          div >
+        <
+            h1 > Top 5 CSS Colors < /h1> <
+          ol > { /* display the array of <li> elements here! */ } { userHeadings } <
+        /ol> < /
+          div >
   );
 }
 
